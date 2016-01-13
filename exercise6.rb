2c0ffbd43aa30ab46f96fdfa9d58e3be7035201b
frances conley wood <f.conleywood@gmail.com>
2016-01-13 14:51:28 -0500

    Part 4 Exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -24,3 +24,13 @@ asterisk_list(grocery_list)
 # Better output the total number of items on your list.
 
 puts grocery_list.count
+
+# Check to see if your list includes "bananas". If it does, output
+# "You need to pick up bananas". Otherwise, output "You don't need to pick up
+#  bananas today".
+
+if grocery_list.include?("bananas") == true
+      puts "You need to pick up bananas"
+    else
+      puts "You don't need to pick up bananas today"
+    end
