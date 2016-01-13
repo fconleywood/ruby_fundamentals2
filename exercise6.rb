b125ac1ec16891d36e131507f512489db809fa49
frances conley wood <f.conleywood@gmail.com>
2016-01-13 13:52:14 -0500

    Part 2 Exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,10 @@
 # We're going to make a shopping list by storing a few items in an array
 
 grocery_list = ["apples", "oranges", "lemons", "grapes"]
+
+# Present your list with an item on each line, with an asterisk (*)
+# in front of it
+
+grocery_list.each do |grocery|
+      puts "*" + grocery
+      end
